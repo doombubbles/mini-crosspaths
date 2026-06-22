f33fa92eb9fed55bd1152d38e18c14a56d2be4b6
doombubbles <obsidiantoolzg@gmail.com>
2026-06-22 09:56:45 -0700

    Bump version

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -2,8 +2,8 @@ namespace MiniCrosspaths;
 
 public static class ModHelperData
 {
-    public const string WorksOnVersion = "54.0";
-    public const string Version = "1.0.0";
+    public const string WorksOnVersion = "55.0";
+    public const string Version = "1.0.1";
     public const string Name = "Mini Crosspaths";
 
     public const string Description =
